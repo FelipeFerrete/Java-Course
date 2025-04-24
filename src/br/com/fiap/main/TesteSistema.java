cbb83ed2d60950169ae6ec3f4123fd8fecf4d5ed
FelipeFerrete <felipeferretelemes@gmail.com>
2025-04-24 09:33:33 -0300

    Saidas Class Main

--- src/br/com/fiap/main/TesteSistema.java
+++ src/br/com/fiap/main/TesteSistema.java
@@ -1,8 +1,34 @@
 package br.com.fiap.main;
 
+import br.com.fiap.beans.Produto;
+
+import javax.swing.*;
+
 public class TesteSistema {
+
+        static String texto(String j){
+            return JOptionPane.showInputDialog(j);
+        }
+
+
+        static int inteiro(String j){
+            return Integer.parseInt(JOptionPane.showInputDialog(j));
+        }
+
+        static double real(String j){
+            return Double.parseDouble(JOptionPane.showInputDialog(j));
+        }
+
     public static void main(String[] args) {
+        Produto objProduto = new Produto();
 
+        objProduto.setCodigo(inteiro("Informe o codigo:"));
+        objProduto.setTipo(texto("Tipo Produto"));
+        objProduto.setMarca(texto("Marca"));
+        objProduto.setQuantidade(inteiro("Quantidade"));
+        objProduto.setValor(real("Valor"));
 
+        System.out.println(objProduto);
     }
 }
+

be8ba965160ce8fda3f6ec3783954e9bb39c0e89
FelipeFerrete <felipeferretelemes@gmail.com>
2025-04-24 08:45:42 -0300

    Criacao de classe + Atributos

---
+++ src/br/com/fiap/beans/Produto.java
@@ -0,0 +1,9 @@
+package br.com.fiap.beans;
+
+public class Produto {
+    private int codigo;
+    private String tipo;
+    private String marca;
+    private int quantidade;
+    private double valor;
+}
